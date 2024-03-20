f525bf8ec60ef809a63c2be90dc667f50624b52c
chiloc121100 <109579264+chiloc121100@users.noreply.github.com>
2024-03-21 04:17:43 +0700

    structure

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SchoolProject1640.Models;
+using System.Data;
+
+namespace SchoolProject1640.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ApplicationUser> User { get; set; } 
+        public DbSet<Faculty> Faculty { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationRole>().HasData(
+                new ApplicationRole { Id = "1", Name = "Guest", Descriptions = "Guest role description", ConcurrencyStamp = "1", NormalizedName = "GUEST" },
+                new ApplicationRole { Id = "2", Name = "Student", Descriptions = "Student role description", ConcurrencyStamp = "2", NormalizedName = "STUDENT" },
+                new ApplicationRole { Id = "3", Name = "Manager", Descriptions = "Manager role description", ConcurrencyStamp = "3", NormalizedName = "MANAGER" },
+                new ApplicationRole { Id = "4", Name = "Administrator", Descriptions = "Admin role description", ConcurrencyStamp = "4", NormalizedName = "ADMINISTRATOR" },
+                new ApplicationRole { Id = "5", Name = "Coordinator", Descriptions = "Coordinator role description", ConcurrencyStamp = "5", NormalizedName = "COORDINATOR" }
+            );
+
+            modelBuilder.Entity<Faculty>().HasData(
+                new Faculty { Id = "1", Name = "Arts", Description = "Faculty of Arts", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now },
+                new Faculty { Id = "2", Name = "Business", Description = "Faculty of Business", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now },
+                new Faculty { Id = "3", Name = "Engineering", Description = "Faculty of Engineering", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now },
+                new Faculty { Id = "4", Name = "Information Technology", Description = "Faculty of Information Technology", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now },
+                new Faculty { Id = "5", Name = "Law", Description = "Faculty of Law", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now },
+                new Faculty { Id = "6", Name = "Medicine", Description = "Faculty of Medicine", CreatedAt = DateTime.Now, UpdatedAt = DateTime.Now }
+            );
+        }
+
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolProject1640.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        // Add any additional properties or methods here
+        public string? FacultyId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
