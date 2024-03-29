b2547531307a29c3e4cb8383a6bcf7547521dc6c
Juno270302 <hungnxb2703@gmail.com>
2024-03-29 13:17:19 +0700

    JunoUpdate - Frontend - Login, Register, Narbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
     "./Areas/**/**/**/**/*.cshtml",
   ],
   theme: {
-    extend: {},
+      extend: {
+          screens: {
+              "2xl": { max: "1535px" },
+              // => @media (max-width: 1535px) { ... }
+
+              xl: { max: "1279px" },
+              // => @media (max-width: 1279px) { ... }
+
+              lg: { max: "1000px" },
+              // => @media (max-width: 1023px) { ... }
+
+              md: { max: "767px" },
+              // => @media (max-width: 767px) { ... }
+
+              sm: { max: "639px" },
+              // => @media (max-width: 639px) { ... }
+          },      },
   },
   plugins: [],
 };
