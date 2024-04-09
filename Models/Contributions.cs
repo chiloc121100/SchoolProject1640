b117050839c979dfb30514fcc6f4ded363c1832a
chiloc121100 <109579264+chiloc121100@users.noreply.github.com>
2024-04-09 11:10:54 +0700

    Revert "Update PK FK"
    This reverts commit 120dabaa85ce7281b7feb32350862e33d33dd838.

--- Models/Contributions.cs
+++ Models/Contributions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SchoolProject1640.Models
 {
@@ -9,8 +8,6 @@ namespace SchoolProject1640.Models
         [Key]
         public int Id { get; set; }
         public string? Title { get; set; }
-        [ForeignKey("FacFContri")]
-        public virtual Faculty? Facultya { get; set; }
         public string? Faculty { get; set; }
         public string? AcademicYear { get; set; }
         public DateTime StartDate { get; set; }
--- Models/Message.cs
+++ Models/Message.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Identity.Client;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace SchoolProject1640.Models
 {
@@ -8,11 +6,7 @@ namespace SchoolProject1640.Models
     {
         [Key]
         public int Id { get; set; }
-        [ForeignKey("UsersFMess")]
-        public virtual ApplicationUser? ApplicationUser { get; set; }
         public string? AccountID { get; set; }
-        [ForeignKey("ArtFTMess")]
-        public virtual Article? Article { get; set; }
         public int? ArtID { get; set; }
         public string? Mess { get; set; }
         public DateTime? CreatedAt { get; set; } = DateTime.Now;
